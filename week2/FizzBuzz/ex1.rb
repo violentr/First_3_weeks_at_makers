d29055a479376b795b3b293553e0e51d33b835c8
Den <den.b@itservicesgoup.co.uk>
2013-08-27 12:59:26 +0100

    Reorganizing structure

---
+++ week2/FizzBuzz/ex1.rb
@@ -0,0 +1,22 @@
+
+n = 1
+j = 100
+string1 ="Fizz"
+string2 ="Buzz"
+string3 ="Fizbuzz"
+
+while n < j
+	
+	n = n + 1 
+
+	if n%3 == 0 && n%5 == 0
+		puts string3
+	elsif n % 3 == 0
+		puts string1
+		elsif n % 5 == 0
+		puts string2
+	else
+		puts n
+	end
+
+end
