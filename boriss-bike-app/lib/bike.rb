6f19e7a655827dcea3e7338feee36b96e77e6a06
Den <den.b@itservicesgoup.co.uk>
2013-09-06 08:47:24 +0100

    Boriss bike app ready

---
+++ boriss-bike-app/lib/bike.rb
@@ -0,0 +1,19 @@
+class Bike
+
+	def initialize broken = false
+		@broken = broken
+	end
+
+	def broken?
+		@broken
+	end
+
+	def break!
+		@broken = true
+	end
+
+	def repair!
+		@broken = false
+	end
+
+end
